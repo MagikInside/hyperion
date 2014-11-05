0860280b6f17e2bf3d13777e6f324d2bb29ea3e5
Magik Inside <luismamoran@gmail.com>
2014-11-05 13:53:21 +0100

    Test with Express and MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express(),
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server;
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+var db;
+
+app.get('*', function (req, res) {
+    db.collection('users').findOne({}, function(err, doc) {
+        if(err) throw err;
+        res.send("Hola " + doc.user);
+    });
+});
+// Connect to the db and start server
+MongoClient.connect("mongodb://root:S71AEkqlorN8@127.0.0.1/test", function(err, database) {
+    if (err) throw err;
+    db=database;
+    app.listen(3000,function(){
+        console.log('Express server running on port 3000..');
+    });
 });
 
-app.listen(3000);
