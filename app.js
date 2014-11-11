56e3c3dcdffae45344d9255a2cddff270775957b
Magik Inside <luismamoran@gmail.com>
2014-11-11 13:45:31 +0100

    Serve static page, add morgan

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 var express = require('express'),
+    morgan = require('morgan'),
+//    bodyParser = require('body-parser'),
+//    methodOverride = require('method-override'),
     app = express(),
     MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server;
+    Server = require('mongodb').Server,
+    ObjectID = require('mongodb').ObjectID;
 
+var url_local = "mongodb://127.0.0.1/test";
+var url_cloud = "mongodb://root:S71AEkqlorN8@127.0.0.1/test";
+
+//The database
 var db;
 
-app.get('*', function (req, res) {
-    db.collection('users').findOne({}, function(err, doc) {
-        if(err) throw err;
-        //res.send("Hola " + doc.user);
-
-    });
-});
 // Connect to the db and start server
-//MongoClient.connect("mongodb://root:S71AEkqlorN8@127.0.0.1/test", function(err, database) {
-MongoClient.connect("mongodb://127.0.0.1/test", function(err, database) {
+MongoClient.connect(url_cloud, function(err, database) {
     if (err) throw err;
-    db=database;
-    app.listen(3000,function(){
+    db = database;
+    app.listen(3000, function () {
         console.log('Express server running on port 3000..');
     });
 });
+
+// Configuration
+app.use(express.static(__dirname + '/public')); 	// set the static files location
+app.use(morgan('dev')); 					// log every request to the console
+//    app.use(bodyParser()); 						// pull information from html in POST
+//    app.use(methodOverride()); 					// simulate DELETE and PUT
+
+
+
+
+// Route to the page
+app.get('*', function(req, res) {
+    // res.sendFile('/home/luisma/workspace/pruebaMEAN/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 
