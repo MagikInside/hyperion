d13881a93468952ff508190d64f243c5111c5b65
Magik Inside <luismamoran@gmail.com>
2014-11-11 19:56:42 +0100

    Add get route to fetch info of the cities form the db

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ var express = require('express'),
     Server = require('mongodb').Server,
     ObjectID = require('mongodb').ObjectID;
 
-var url_local = "mongodb://127.0.0.1/test";
+var url_local = "mongodb://127.0.0.1/infojobs";
 var url_cloud = "mongodb://client:mongo1184@127.0.0.1/test";
 
 //The database
 var db;
 
 // Connect to the db and start server
-MongoClient.connect(url_cloud, function(err, database) {
+MongoClient.connect(url_local, function(err, database) {
     if (err) throw err;
     db = database;
     app.listen(3000, function () {
@@ -30,7 +30,18 @@ app.use(morgan('dev')); 					// log every request to the console
 
 
 
-
+//Routes
+//GET -
+app.get('/api/cities', function(req, res) {
+    db.collection('jobs').aggregate([
+        { $group:{ _id:"$city",num:{$sum:1}}},
+        {$sort:{num:-1}},
+        {$limit:10}
+    ], function(err, cities) {
+        if(err) throw err;
+        res.json(cities);
+    });
+});
 // Route to the page
 app.get('*', function(req, res) {
     // res.sendFile('/home/luisma/workspace/pruebaMEAN/public/index.html');
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,6 +3,17 @@
  */
 var hyperion = angular.module('hyperion', []);
 
-hyperion.controller('MainCtrl', function ($scope) {
+hyperion.controller('MainCtrl', function ($scope,  $http) {
     $scope.name = 'MaGiK';
+
+    // CWhen the page loads, ask for the data
+    $http.get('/api/cities')
+        .success(function(data) {
+            $scope.cities = data;
+           // console.log(data)
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
 });
